8ea5a9304d06cb5f95f1203e319e5f0006471eee
Dilara <dcavus@gmail.com>
2023-11-28 15:03:03 -0800

    appointment application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export makes the class accessible
 export class AppComponent {
   title = 'appointment-app';
+  items: string[]=['item1','item2'];
+
+// no need for return type
+  log(text: string): void {
+    var message: string='Message '+text;
+    console.log(message)
+  }
+// no need for return type
+  sum(a:number, b:number): number{
+    return a+b;
+  }
+  f(){
+    this.log('a')
+    this.log('b')
+  }
+  // number, string, boolean primitive datatypes in Typescript
+  // var app : AppComponent=new AppComponent();
+
+
+
 }
