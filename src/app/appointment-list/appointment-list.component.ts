e34ca7949672fadc79aaa7c681c4b8bd5c159eae
Dilara <dcavus@gmail.com>
2023-12-22 13:21:19 -0800

    appointment object added

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Appointment } from '../models/appointment';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+  // it is property since it is defined inside a class
+  // when it is defined inside a function/method it is a variable
+  appointment: Appointment ={
+    id: 1,
+    title: "Take dog for a walk",
+    date: new Date('2023-12-22')
+  }
 
 }
---
+++ src/app/models/appointment.ts
@@ -0,0 +1,7 @@
+// since we will use it out side we need to export it- 
+// we are making it importable
+export interface Appointment {
+    id: number,
+    title: string,
+    date: Date
+}
